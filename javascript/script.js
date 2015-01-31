68604bef0e994adb84fb5bf205e8f99a41b4a702
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 13:08:38 -0800

    show matches

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,8 @@ function myFunction() {
 	var regex = /asian/g;
     var x = document.getElementById("content-area").value;
 
-        while ((match = regex.exec(x)) !== null) {
-    	    console.log(match);
-    	}
+    var match = regex.exec(x);
+    console.log(match);
 
 
 }
