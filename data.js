1ddff6b474f3dc1f34edd67333721f483b474fef
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-31 15:18:53 -0800

    Added more test data.

---
+++ data.js
@@ -0,0 +1,62 @@
+var data =
+  [
+    {
+      "id": 0,
+      "categoryName": "abelism",
+      "subcategories": [
+        {
+          "subID": 0,
+          "name": "aaa",
+          "keywords": [
+            {
+              "keywordText": "lame",
+              "links": [
+                "http://kronda.com/thats-so-lame-why-i-stopped-using-lame",
+                "http://asusequity.wordpress.com/2011/09/09/why-i-dont-say-lame/"
+              ]
+            },
+            {
+              "keywordText": "retard",
+              "links": [
+                "http://www.r-word.org"
+              ]
+            },
+            {
+              "keywordText": "crippled",
+              "links": [
+                "http://everydayfeminism.com/2014/11/ableist-language-matters/"
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "categoryName": "gender",
+      "subcategories": [
+        {
+          "subID": 0,
+          "name": "bbb",
+          "keywords": [
+            {
+              "keywordText": "mankind",
+              "links": ['']
+            },
+            {
+              "keywordText": "bi-sexual",
+              "links": ['']
+            },
+            {
+              "keywordText": "bisexual",
+              "links": ['']
+            },
+            {
+              "keywordText": "bi sexual",
+              "links": ['']
+            }
+          ]
+        }
+      ]
+    }
+  ];
