5f8485f75be1f5dcb96f75e48bf8c7808a0a68b0
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 13:31:02 -0800

    correct stuff

--- javascript/script.js
+++ javascript/script.js
@@ -7,9 +7,10 @@ function myFunction() {
 
     var race = ['asian','hispanic','indian','european'];
 
-    console.log(match);
+    console.log(match[0]);
+
     for( var i = 0; race[i] <= race.length(); i++) {
-    	if ( match === race[0] ) {
+    	if ( match[i] === race[i] ) {
     		document.getElementById("message").innerHTML = '<p class="notes">Found asian keyword</p>';
     	}
     }
