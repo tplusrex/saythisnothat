9cd47882074f4d2221a5c4dc1d0e19faddb4516f
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 13:52:07 -0800

    Finally array

--- javascript/script.js
+++ javascript/script.js
@@ -3,11 +3,19 @@ function myFunction() {
 	var regex = /asian/g;
     var x = document.getElementById("content-area").value;
 
-    var match;
-
-	while ( (match = regex.exec(x)) !== null )  {  
-	  console.log(match[1]);
-	}
+    var match = regex.exec(x);
+    var output = x.match(/asian/g);
+    // var race = ['asian','hispanic','indian','european'];
+    console.log(match[0]);
+    console.log(match);
+    console.log(output);
+    console.log(output[0]);
+    
+    // for( var i = 0; race[i] <= race.length(); i++) {
+    // 	if ( match[i] === race[i] ) {
+    // 		document.getElementById("message").innerHTML = '<p class="notes">Found asian keyword</p>';
+    // 	}
+    // }
 
 
 }
