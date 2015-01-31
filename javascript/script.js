f02713ce7aeeeaabd6e7b497c3bca690e5ffe6e4
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 12:42:13 -0800

    Add JS

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+
+    var x = document.getElementById("shortcode-textarea").value;
+    var match = regex.exec(x);
+
+    console.log(match[0]);
+
+    // document.getElementById("preview").innerHTML = 'ಠ_ಠ';
+
+
+}
