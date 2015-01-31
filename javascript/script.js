50bbe02d46afb08a486a7e3e798224b9b9a41099
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 13:06:33 -0800

    Print array

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,9 @@ function myFunction() {
 
 	var regex = /asian/g;
     var x = document.getElementById("content-area").value;
-    
+
         while ((match = regex.exec(x)) !== null) {
-    	    console.log(match[0]);
+    	    console.log(match);
     	}
 
 
