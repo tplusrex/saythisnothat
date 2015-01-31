084adabad5aa8054f6eddfdf920221a3ff16a107
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 14:35:00 -0800

    Add Classes

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,19 @@
 function myFunction() {
 
-	var regex = /asian|abelism|gender|transgendered/g;
+	var regex = /lame|retard/g;
     var x = document.getElementById("content-area").value;
 
     var output = x.match(regex);
 
     for ( var i=0; i < output.length; i++ ) {
     	var message;
-    	if (output[i] === 'asian') {
+    	if (output[i] === 'lame') {
     		message = 'this is a message for asian';
     	}
-    	if (output[i] === 'gender') {
+    	if (output[i] === 'retard') {
     		message = 'this is a message for gender';
     	}
-    	$("#messages").append('<li>' + output[i] + ' ' + message + '</li>');
+    	$("#messages").append('<li><span class="word">' + output[i] + '</span> <span class="message">' + message + '</span></li>');
     }
     console.log(output);
     console.log(output.length);
