877eafb4dba0b222d14c75babbd95fc896909e59
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-31 15:34:10 -0800

    Removing subcategories. Set the three main categories and added more keywords.

--- data.js
+++ data.js
@@ -2,60 +2,82 @@ var data =
   [
     {
       "id": 0,
-      "categoryName": "abelism",
-      "subcategories": [
+      "categoryName": "Identities",
+      "keywords": [
         {
-          "subID": 0,
-          "name": "aaa",
-          "keywords": [
-            {
-              "keywordText": "lame",
-              "links": [
-                "http://kronda.com/thats-so-lame-why-i-stopped-using-lame",
-                "http://asusequity.wordpress.com/2011/09/09/why-i-dont-say-lame/"
-              ]
-            },
-            {
-              "keywordText": "retard",
-              "links": [
-                "http://www.r-word.org"
-              ]
-            },
-            {
-              "keywordText": "crippled",
-              "links": [
-                "http://everydayfeminism.com/2014/11/ableist-language-matters/"
-              ]
-            }
+          "keywordText": "lame",
+          "links": [
+            "http://kronda.com/thats-so-lame-why-i-stopped-using-lame",
+            "http://asusequity.wordpress.com/2011/09/09/why-i-dont-say-lame/"
+          ]
+        },
+        {
+          "keywordText": "retard",
+          "links": [
+            "http://www.r-word.org"
+          ]
+        },
+        {
+          "keywordText": "crippled",
+          "links": [
+            "http://everydayfeminism.com/2014/11/ableist-language-matters/"
           ]
         }
       ]
     },
     {
       "id": 1,
-      "categoryName": "gender",
-      "subcategories": [
+      "categoryName": "Gender Inclusion",
+      "keywords": [
         {
-          "subID": 0,
-          "name": "bbb",
-          "keywords": [
-            {
-              "keywordText": "mankind",
-              "links": ['']
-            },
-            {
-              "keywordText": "bi-sexual",
-              "links": ['']
-            },
-            {
-              "keywordText": "bisexual",
-              "links": ['']
-            },
-            {
-              "keywordText": "bi sexual",
-              "links": ['']
-            }
-          ]
+          "keywordText": "mankind",
+          "links": ['']
+        },
+        {
+          "keywordText": "bi-sexual",
+          "links": ['']
+        },
+        {
+          "keywordText": "bisexual",
+          "links": ['']
+        },
+        {
+          "keywordText": "bi sexual",
+          "links": ['']
+        },
+        {
+          "keywordText": "he",
+          "links": ['']
+        },
+        {
+          "keywordText": "she",
+          "links": ['']
+        },
+        {
+          "keywordText": "father",
+          "links": ['']
+        },
+        {
+          "keywordText": "mother",
+          "links": ['']
+        },
+        {
+          "keywordText": "transgendered",
+          "links": ['']
+        },
+        {
+          "keywordText": "transgender",
+          "links": ['']
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "categoryName": "Militaristic",
+      "keywords": [
+        {
+          "keywordText": "war on",
+          "links": []
         }
       ]
     }
