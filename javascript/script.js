4ee457180194148b88d58176585a9fa9a868feae
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 15:15:00 -0800

    message

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,15 @@
 function myFunction() {
 
-	var regex = /lame|retard/g;
+	var regex = /(lame|retard)/g;
     var x = document.getElementById("content-area").value;
 
     var output = x.match(regex);
 
-    for ( var i=0; i < output.length; i++ ) {
+    var summary = x.replace(regex,'<span class=' + '$1' + '>' + '$1' + '</span>');
+
+    $("#summary").append('<p>' + summary + '</p>');
+
+    for ( var i = 0; i < output.length; i++ ) {
     	var message;
     	if (output[i] === 'lame') {
     		message = 'this is a message for asian';
@@ -15,6 +19,7 @@ function myFunction() {
     	}
     	$("#messages").append('<li><span class="word">' + output[i] + '</span> <span class="message">' + message + '</span></li>');
     }
+
     console.log(output);
     console.log(output.length);
 
