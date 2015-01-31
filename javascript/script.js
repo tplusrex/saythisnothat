9e726fb86b93033a1ad76118c192e0199f6045f3
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 13:28:39 -0800

    Add loop

--- javascript/script.js
+++ javascript/script.js
@@ -4,10 +4,15 @@ function myFunction() {
     var x = document.getElementById("content-area").value;
 
     var match = regex.exec(x);
-    console.log(match);
 
-    if( match === 'asian' ) {
-    	document.getElementById("message").innerHTML = '<p class="notes">Found asian keyword</p>';
+    var race = ['asian','hispanic','indian','european'];
+
+    console.log(match);
+    for( var i = 0; race[i] <= race.length(); i++) {
+    	if ( match === race[0] ) {
+    		document.getElementById("message").innerHTML = '<p class="notes">Found asian keyword</p>';
+    	}
     }
+
 
 }
