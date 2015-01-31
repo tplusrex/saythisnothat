3d7ec231bf1e4027aa1b436ee48e16d1dceda39a
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 14:05:14 -0800

    JS Update

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,11 @@
 function myFunction() {
 
-	var regex = /asian/g;
+	var regex = /asian|abelism|gender|transgendered/g;
     var x = document.getElementById("content-area").value;
 
-    var match = regex.exec(x);
-    var output = x.match(/asian/g);
-    // var race = ['asian','hispanic','indian','european'];
-    console.log(match[0]);
-    console.log(match);
-    console.log(output);
-    console.log(output[0]);
-    
-    // for( var i = 0; race[i] <= race.length(); i++) {
-    // 	if ( match[i] === race[i] ) {
-    // 		document.getElementById("message").innerHTML = '<p class="notes">Found asian keyword</p>';
-    // 	}
-    // }
+    var output = x.match(regex);
 
+    console.log(output);
+    console.log(output.length);
 
 }
