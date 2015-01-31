343c493f816321175dbd55adf83964510211147a
Tony Vongprachanh <tvongpr@gmail.com>
2015-01-31 14:30:03 -0800

    Add looper

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,16 @@ function myFunction() {
 
     var output = x.match(regex);
 
+    for ( var i=0; i < output.length; i++ ) {
+    	var message;
+    	if (output[i] === 'asian') {
+    		message = 'this is a message for asian';
+    	}
+    	if (output[i] === 'gender') {
+    		message = 'this is a message for gender';
+    	}
+    	$("#messages").append('<li>' + output[i] + ' ' + message + '</li>');
+    }
     console.log(output);
     console.log(output.length);
 
