fe6bb095b8a1b0df626ed97105d88d5c3d8e3871
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-31 16:10:20 -0800

    Added alternatives, added more data.

--- javascript/data.js
+++ javascript/data.js
@@ -5,7 +5,36 @@ var data =
       "categoryName": "Identities",
       "keywords": [
         {
+          "keywordText": "achievement gap",
+          "alternatives": [ "opportunity gap" ],
+          "links": [
+            "http://magazine.good.is/articles/please-stop-using-the-phrase-achievement-gap"
+          ]
+        },
+        {
+          "keywordText": "anchor babies",
+          "alternatives": [ "child of undocumented immigrant" ],
+          "links": [
+            "https://www.ahdictionary.com/word/search.html?q=anchor+baby&submit.x=-786&submit.y=-210"
+          ]
+        },
+        {
+          "keywordText": "at-risk youth",
+          "alternatives": [ "at-promise youth" ],
+          "links": [
+            "http://voices.washingtonpost.com/class-struggle/2009/11/post_1.html"
+          ]
+        },
+        {
+          "keywordText": "at risk youth",
+          "alternatives": [ "at-promise youth" ],
+          "links": [
+            "http://voices.washingtonpost.com/class-struggle/2009/11/post_1.html"
+          ]
+        },
+        {
           "keywordText": "lame",
+          "alternatives": [ "uncool" ],
           "links": [
             "http://kronda.com/thats-so-lame-why-i-stopped-using-lame",
             "http://asusequity.wordpress.com/2011/09/09/why-i-dont-say-lame/"
@@ -13,16 +42,32 @@ var data =
         },
         {
           "keywordText": "retard",
+          "alternatives": [ "jerk", "stupid", "annoying", "nincompoop", "twit" ],
+          "links": [
+            "http://www.r-word.org"
+          ]
+        },
+        {
+          "keywordText": "retarded",
+          "alternatives": [ "jerk", "stupid", "annoying", "nincompoop", "twit" ],
           "links": [
             "http://www.r-word.org"
           ]
         },
         {
           "keywordText": "crippled",
+          "alternatives": [ "jerk", "stupid", "annoying", "nincompoop" ],
           "links": [
             "http://everydayfeminism.com/2014/11/ableist-language-matters/"
           ]
-        }
+        },
+        {
+          "keywordText": "insane",
+          "alternatives": [ "illogical", "irrational misleading", "lying", "not thinking", "incapable of critical thinking", "an asshat", "a dipshit", "irrelevant", "rationalizing", "weird" ],
+          "links": [
+            "http://everydayfeminism.com/2014/11/ableist-language-matters/"
+          ]
+        },
       ]
     },
     {
@@ -31,53 +76,74 @@ var data =
       "keywords": [
         {
           "keywordText": "mankind",
-          "links": ['']
+          "alternatives": [""],
+          "links": [""]
         },
         {
           "keywordText": "bi-sexual",
-          "links": ['']
+          "alternatives": [""],
+          "links": [""]
         },
         {
           "keywordText": "bisexual",
-          "links": ['']
+          "alternatives": [""],
+          "links": [""]
         },
         {
           "keywordText": "bi sexual",
-          "links": ['']
+          "alternatives": [""],
+          "links": [""]
         },
         {
           "keywordText": "he",
-          "links": ['']
+          "alternatives": [ "they" ],
+          "links": [""]
         },
         {
           "keywordText": "she",
-          "links": ['']
+          "alternatives": [ "they" ],
+          "links": [""]
         },
         {
           "keywordText": "father",
-          "links": ['']
+          "alternatives": [ "parent", "guardian" ],
+          "links": [""]
         },
         {
           "keywordText": "mother",
-          "links": ['']
+          "alternatives": [ "parent", "guardian" ],
+          "links": [""]
         },
         {
           "keywordText": "transgendered",
-          "links": ['']
+          "alternatives": [""],
+          "links": [""]
         },
         {
           "keywordText": "transgender",
-          "links": ['']
-        }
+          "alternatives": [""],
+          "links": [""]
+        },
+        {
+          "keywordText": "throw like a girl",
+          "alternatives": [""],
+          "links": ["http://www.slate.com/blogs/xx_factor/2014/09/11/james_brown_on_domestic_violence_the_cbs_sportscaster_delivers_a_powerful.html"]
+        },
+        {
+          "keywordText": "throw like a sissy",
+          "alternatives": [""],
+          "links": ["http://www.slate.com/blogs/xx_factor/2014/09/11/james_brown_on_domestic_violence_the_cbs_sportscaster_delivers_a_powerful.html"]
+        },
       ]
     },
     {
       "id": 2,
-      "categoryName": "Militaristic",
+      "categoryName": "Militaristic Language",
       "keywords": [
         {
           "keywordText": "war on",
-          "links": ['']
+          "alternatives": [ "initiative against", "effort towards" ],
+          "links": [""]
         }
       ]
     }
