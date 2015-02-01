d02963ea130011e0d47190185cc8a267071f3721
Shweta Panditrao <panditraos@yahoo-inc.com>
2015-01-31 16:25:40 -0800

    updates

--- javascript/script.js
+++ javascript/script.js
@@ -10,14 +10,20 @@ if ($(this).scrollTop() > 1){
 function myFunction() {
     var text = document.getElementById("content-area").value;
 
+    var message = '';
+
     var i;
     for (i = 0; i < data.length; i++) {
         var keywords = data[i].keywords;
+        message += '<h2>' + data[i].categoryName + '</h2>';
         var j;
         for (j = 0; j < keywords.length; j++) {
             var keyword = keywords[j].keywordText;
             var index = text.indexOf(keyword);
             if (index != -1) {
+                 message += '<h3>' + keyword + '</h3>';
+                message += '<span>Suggested Alternatives: ' + data[i].keywords[j].alternatives.join(', ') + '</span>';
+                message += '<span>More Information at: ' + data[i].keywords[j].links.join(', ') + '</span>';
                 var preText = '<span class="' + data[i].categoryName.toLowerCase().replace(/\s+/g, '') + '">';
                 var postText = '</span>';
                 text = [text.slice(0, index), preText, keyword, postText, text.slice(index+keyword.length)].join('');
@@ -25,7 +31,7 @@ function myFunction() {
         }
     }
 
-    $("#summary").append('<p>' + text + '</p>');
+    $("#summary").append(text + '<br/><br/>' + message);
 
 
 
